d4f0afd8b25a5b9dc29aa24dd868bb6d81e77729
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-05 09:28:27 -0800

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordReverse wordreverse = new WordReverse();
+
+            Console.WriteLine(wordreverse.ReverseWord("Hello World"));
+
+        }
+
+        
+    }
+
+    
+}
---
+++ ConsoleApp4/WordReverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class WordReverse
+    {
+        public string ReverseWord(string str)
+        {
+            string result = String.Empty;
+
+            string[] arr = str.Split(' ');
+
+            foreach (string word in arr)
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    result += word[i];
+
+                }
+                if (result.Length < str.Length)
+                    result += " ";
+            }
+            return result;
+
+
+        }
+    }
+}
